c8cb022efda0b044fa9632243e390f8d8e4f7d74
arrizkyhp <52059831+arrizkyhp@users.noreply.github.com>
2022-01-24 17:24:00 +0700

    Update Section 10 part 64 (get user repos)

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -10,6 +10,7 @@ export const GithubProvider = ({children}) => {
     const intialState = {
         users: [],
         user: {},
+        repos: [],
         loading: false
     }
 
@@ -52,6 +53,7 @@ export const GithubProvider = ({children}) => {
         })
     }
 
+
     // Get Single user
     const getUser = async  (login) => {
         setLoading()
@@ -73,7 +75,29 @@ export const GithubProvider = ({children}) => {
             })
         }
     }
+    
+    // Get user repos
+    const getUserRepos = async  (login) => {
+        setLoading()
 
+        const params = new URLSearchParams({
+            sort: 'created',
+            per_page: 10
+        })
+  
+        const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
+          headers: {
+            Authorization: `token ${GITHUB_TOKEN}`
+          }
+        })
+    
+        const data = await response.json()
+    
+        dispatch({
+            type: 'GET_REPOS',
+            payload: data
+        })
+    }
 
 
     // Clear result
@@ -86,9 +110,11 @@ export const GithubProvider = ({children}) => {
         users: state.users, 
         user: state.user,
         loading: state.loading, 
+        repos: state.repos,
         searchUsers, 
         clearUsers,
-        getUser
+        getUser,
+        getUserRepos,
     }}>
         {children}
     </GithubContext.Provider>
